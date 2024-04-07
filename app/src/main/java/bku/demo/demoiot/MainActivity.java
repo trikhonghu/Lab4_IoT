ebf8d35db37f9213a166ad6b330043750ee88b5c
trikhonghu <113248981+trikhonghu@users.noreply.github.com>
2024-04-07 19:32:36 +0700

    Demo

---
+++ app/src/main/java/bku/demo/demoiot/MainActivity.java
@@ -0,0 +1,14 @@
+package bku.demo.demoiot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
